ea5fe5462457d8c7823bbbcd0a518428952a73ec
Pranav Sharma <pransharm11@gmail.com>
2021-08-27 18:32:06 +0530

    add csv parser

---
+++ parser.h
@@ -0,0 +1,17 @@
+#ifndef PARSER_H_
+#define PARSER_H_
+#include<string>
+#include<iterator>
+#include<fstream>
+#include<vector>
+namespace parser {
+    enum class CSVState {
+        UnquotedField,
+        QuotedField,
+        QuotedQuote
+    };
+    std::vector<std::string> readCSVRow(const std:: string& row);
+    std::vector<std::vector<std::string>> readCSVFile(std::string fileName);
+}
+
+#endif
