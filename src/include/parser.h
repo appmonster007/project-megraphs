c38a6a47f69b491ec553bf55c307d93c3b40d30d
prathitaswar <prathitaswar@Prathits-MacBook-Pro.local>
2021-09-08 16:43:21 +0530

    experimented with finding out eigen values and vectors

--- src/include/parser.h
+++ src/include/parser.h
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <vector>
 #include <Eigen/Sparse>
+#include <Eigen/Dense>
 namespace parser
 {
     enum class CSVState
@@ -15,8 +16,10 @@ namespace parser
     };
     std::vector<std::string> readCSVRow(const std::string &row);
     Eigen::SparseMatrix<double> readCSVFile(std::string &fileName);
+    Eigen::MatrixXd readCSVFiletoDense(std::string &fileName);
     Eigen::SparseMatrix<double> readMTXFile(std::string &fileName);
     Eigen::SparseMatrix<double> readMTXFile(std::string &fileName, int mode);
+    Eigen::MatrixXd readMTXFiletoDense(std::string &fileName);
 }
 
 #endif
--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+// #include <Eigen/Eigenvalues>
 #include <Spectra/GenEigsSolver.h>
 #include <Spectra/SymEigsSolver.h>
 #include <Spectra/MatOp/SparseGenMatProd.h>
@@ -13,58 +14,63 @@ int main()
     M_file = "../assets/M_web-edu.csv";
     L_file = "../assets/L_tech-internet-as.mtx";
 
-    Eigen::SparseMatrix<double> XS_mat(5,5);
-    for(int i=0; i<5; i++){
-        XS_mat.insert(i, i) = 2;
-    }
-    XS_mat.insert(0, 1) = 1;
-    XS_mat.insert(3, 4) = 1;
+    // Eigen::MatrixXd S_mat = Eigen::MatrixXd::Zero(10, 10);
+    // for(int i=0; i<10; i++){
+    //     for(int j=0; j<10; j++){
+    //         S_mat(i, j) = 1;
+    //     }
+    // }
 
-    // std::cout<<XS_mat<<std::endl;
+    // Eigen::MatrixXd S_mat = parser::readMTXFiletoDense(L_file);
+    Eigen::MatrixXd S_mat = parser::readCSVFiletoDense(M_file);
+    // std::cout << S_mat << "\n";
+    Eigen::EigenSolver<Eigen::MatrixXd> es(S_mat, false);
+    std::cout << "The eigenvalues of the 10x10 matrix of ones are:" 
+     << std::endl << es.eigenvalues() << std::endl;
 
-    Eigen::SparseMatrix<double> S_mat = parser::readMTXFile(S_file);
     // // Eigen::SparseMatrix<double> * d = new Eigen::SparseMatrix<double>(data);
     // Eigen::SparseMatrix<double> M_mat = parser::readCSVFile(M_file);
     // Eigen::SparseMatrix<double> L_mat = parser::readMTXFile(L_file);
 
-    Spectra::SparseGenMatProd<double> XS_op(XS_mat);
-    Spectra::SparseSymMatProd<double> S_op(S_mat);
+    // Spectra::SparseGenMatProd<double> XS_op(XS_mat);
+    // Spectra::SparseSymMatProd<double> S_op(S_mat);
     // Spectra::SparseSymMatProd<double> M_op(M_mat);
     // Spectra::SparseSymMatProd<double> L_op(L_mat);
 
     // Construct eigen solver object, requesting the largest three eigenvalues
-    Spectra::GenEigsSolver<Spectra::SparseGenMatProd<double>> XS_eigs(XS_op, 3, 5);
-    Spectra::SymEigsSolver<Spectra::SparseSymMatProd<double>> S_eigs(S_op, 3, 6);
+    // Spectra::GenEigsSolver<Spectra::SparseGenMatProd<double>> XS_eigs(XS_op, 8, 10);
+    // Spectra::SymEigsSolver<Spectra::SparseSymMatProd<double>> S_eigs(S_op, 33, 34);
     // Spectra::SymEigsSolver<Spectra::SparseSymMatProd<double>> M_eigs(M_op, 3, 6);
     // Spectra::SymEigsSolver<Spectra::SparseSymMatProd<double>> L_eigs(L_op, 3, 6);
 
     // Initialize and compute
-    XS_eigs.init();
-    S_eigs.init();
+    // XS_eigs.init(); 
+    // S_eigs.init();
     // M_eigs.init();
     // L_eigs.init();
 
-    int XS_nconv = XS_eigs.compute(Spectra::SortRule::LargestMagn);
-    int S_nconv = S_eigs.compute(Spectra::SortRule::LargestMagn);
+    // int XS_nconv = XS_eigs.compute(Spectra::SortRule::LargestReal);
+    // int S_nconv = S_eigs.compute(Spectra::SortRule::LargestMagn);
     // int M_nconv = M_eigs.compute(Spectra::SortRule::LargestMagn);
     // int L_nconv = L_eigs.compute(Spectra::SortRule::LargestMagn);
 
     // Retrieve results
-    Eigen::VectorXcd evalues;
+    // Eigen::VectorXcd evalues;
 
-    std::cout << "\nX Small : " << XS_mat.nonZeros()<< "\n";
-    std::cout << XS_eigs.eigenvectors() << "\n";
-    if (XS_eigs.info() == Spectra::CompInfo::Successful)
-        evalues = XS_eigs.eigenvalues();
-    std::cout << "Eigenvalues found for XS :\n"
-              << evalues << std::endl;
+    // std::cout << XS_mat;
+    // std::cout << "\nX Small : " << XS_mat.nonZeros()<< "\n\n";
+    // std::cout << XS_eigs.eigenvectors() << "\n";
+    // if (XS_eigs.info() == Spectra::CompInfo::Successful)
+    //     evalues = XS_eigs.eigenvalues();
+    // std::cout << "Eigenvalues found for XS :\n"
+    //           << evalues << std::endl;
 
-    std::cout << "\nSmall : " << S_mat.nonZeros() / 2 << "\n";
-    std::cout << S_eigs.eigenvectors() << "\n";
-    if (S_eigs.info() == Spectra::CompInfo::Successful)
-        evalues = S_eigs.eigenvalues();
-    std::cout << "Eigenvalues found for S :\n"
-              << evalues << std::endl;
+    // std::cout << "\nSmall : " << S_mat.nonZeros() / 2 << "\n";
+    // std::cout << S_eigs.eigenvectors() << "\n";
+    // if (S_eigs.info() == Spectra::CompInfo::Successful)
+    //     evalues = S_eigs.eigenvalues();
+    // std::cout << "Eigenvalues found for S :\n"
+    //           << evalues << std::endl;
 
     // std::cout << "\nMedium : " << M_mat.nonZeros() / 2 << "\n";
     // if (M_eigs.info() == Spectra::CompInfo::Successful)
