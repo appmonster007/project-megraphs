f6d114b91d094b531617682c6a3fba1d6af3d59d
Suhrid <suhridmulay@gmail.com>
2021-10-26 17:11:24 +0530

    Corrected comments in file

--- Python-Files/Centralities.py
+++ Python-Files/Centralities.py
@@ -2,6 +2,6 @@ from Base import GraphBase
 import networkx as nx
 
 # Sample proposed implementation for centrality
-# To know more check out ./PROPSPEC.md
+# To know more check out SPEC.md in project root
 def degree_centrality(g: GraphBase):
     return nx.degree_centrality(g.graph)
