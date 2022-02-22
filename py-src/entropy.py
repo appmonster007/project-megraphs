58ee8769ca2061e1367292da76e8405993d7b612
Suhrid Mulay <suhridmulay@gmail.com>
2022-02-22 20:45:29 +0530

    Added betweenness distribution entropy

--- py-src/entropy.py
+++ py-src/entropy.py
@@ -1,5 +1,7 @@
+from locale import normalize
 import sys
 import math
+from turtle import clear
 from matplotlib.pyplot import grid
 import networkx
 from Base import graphIO
@@ -24,19 +26,28 @@ def degreeEntropy(graph: networkx.Graph):
     ent = entropy(degrees)
     return ent
 
+def betweenness_entropy(graph: networkx.Graph):
+    betweennesses = networkx.betweenness_centrality(graph, normalized=False)
+    betweennesses = list(dict(betweennesses).values())
+    ent = entropy(betweennesses)
+    return ent;
+
 def test():
     graph = graphIO()
     graph.read_from_mtx_file(sys.argv[1])
+    print(f"[INFO] file read complete")
     networkx_graph = graph.graph
-    original_entropy = degreeEntropy(networkx_graph)
-    print(f"Original Entropy = {original_entropy}")
+    print(f"[INFO] extracted graph")
+    original_entropy = betweenness_entropy(networkx_graph)
+    print(f"Original Entropy = {original_entropy:.2f}")
+
+    
     for node in networkx_graph:
         copy = networkx_graph.copy()
         copy.remove_node(node)
-        copy_entropy = degreeEntropy(copy)
-        print(f"Removing node {node} Resultant entropy: {copy_entropy}")
-        print(f"Node {node} adds {original_entropy - copy_entropy} units of information")
-
+        copy_entropy = betweenness_entropy(copy)
+        print(f"Node: {(node + 1):02}\tDegree: {networkx.degree(networkx_graph, node)} \tDelta H: {(original_entropy - copy_entropy):.2f}\tH after removal: {copy_entropy:.2f}")
+    
         
 
 test()
