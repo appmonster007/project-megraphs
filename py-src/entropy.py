e0ef7332c1bc67ec864b9e8335282c3b579fc648
nitinosiris <nitinwartkar58@gmail.com>
2022-04-07 16:06:10 +0530

    Difference of entropy written

--- py-src/entropy.py
+++ py-src/entropy.py
@@ -5,6 +5,7 @@ from turtle import clear
 from matplotlib.pyplot import grid
 import networkx
 from Base import graphIO
+import copy
 
 def entropy(sample: list):
     distribution = {}
@@ -32,6 +33,21 @@ def betweenness_entropy(graph: networkx.Graph):
     ent = entropy(betweennesses)
     return ent;
 
+# def Diff_of_entropy(graph: networkx.Graph):
+#     original_graph = copy.deepcopy(graph)
+#     original_entropy = betweenness_entropy(original_graph)
+#     for u in graph.copy():
+#         graph.remove_node(u)
+#         copy_entropy = betweenness_entropy(graph)
+#         if(copy_entropy < original_entropy):
+#             # remove that node from original grp
+#             original_graph.remove_node(u)
+#             # recalculate original grps entropy
+#             original_entropy = betweenness_entropy(original_graph)
+#         else:
+#             graph = original_graph.copy()
+#     return original_graph, original_entropy
+
 def test():
     graph = graphIO()
     graph.read_from_mtx_file(sys.argv[1])
@@ -41,7 +57,10 @@ def test():
     original_entropy = betweenness_entropy(networkx_graph)
     print(f"Original Entropy = {original_entropy:.2f}")
 
-    
+    # print("no of nodes before " + str(networkx_graph.number_of_nodes()))
+    # a, b = Diff_of_entropy(networkx_graph)
+    # print("no of nodes after " + str(a.number_of_nodes()))
+    # print(b)
     for node in networkx_graph:
         copy = networkx_graph.copy()
         copy.remove_node(node)
