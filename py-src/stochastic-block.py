8c5a763f649b6ec6aa248eb6fde67fcdde7bba90
Pranav Sharma <43780292+pranav1344@users.noreply.github.com>
2022-04-12 15:16:31 +0530

    Update stochastic-block.py
    Add Poisson noise function.

--- py-src/stochastic-block.py
+++ py-src/stochastic-block.py
@@ -4,6 +4,34 @@ import matplotlib.pyplot as plt
 import scipy
 import scipy.io
 
+import math
+import time
+import random
+
+
+def poisson_noise():
+    INTERVAL = .0005
+    LAMBDA = 10.0
+    events = {}
+
+    def f(x, L):
+      return 1 - math.exp(-L * x)
+
+
+    start = time.time()
+
+    while time.time() - start < 60:
+      if random.random() < f(INTERVAL, LAMBDA):
+        bucket = time.time() - start
+        if bucket in events.keys():
+            events[bucket] += 1
+        else:
+            events[bucket] = 1
+      time.sleep(INTERVAL)
+
+    print(events)
+    return events
+
 sizes = np.random.randint(100,300, 20)
 p = []
 for i in range(0, 20):
