b76dc256b7f17cb6bf758c03d0e59ca97aa47ba0
appmonster007 <snaswar0201@gmail.com>
2024-07-20 11:16:01 +0530

    adding medium size graphs datasets

--- extra/paraquetcsv2mtx.py
+++ extra/paraquetcsv2mtx.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-fld = "assets/reddit_threads/"
+fld = "assets/proteins/"
 
-df = pd.read_csv("assets/converted.csv")
+df = pd.read_json('assets/full.jsonl', lines=True)
 
 df.sort_values(by='num_nodes', inplace=True, ascending=False)
-df.sort_values(by='y', inplace=True, ascending=False)
+# df.sort_values(by='y', inplace=True, ascending=False)
 
 for idx, row in df[:200].iterrows():
     with open(f"{fld}{idx}.mtx", 'w') as file:
         file.write("%%MatrixMarket matrix coordinate pattern symmetric \n")
         nodes = int(row['num_nodes'])
-        edges = eval(row['edge_index'])
+        edges = row['edge_index']
         file.write(f"{nodes} {nodes} {len(edges[0])} \n")
         for i in range(len(edges[0])):
             file.write(f"{edges[0][i]} {edges[1][i]} \n")
